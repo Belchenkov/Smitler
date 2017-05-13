d7be7b9f590ac2f18ebbbab9e7c705507b1deb60
Belchenkov <u608110@gmail.com>
2017-05-13 22:10:58 +0300

    Adaptive Own-Carousel

--- app/js/common.js
+++ app/js/common.js
@@ -10,6 +10,7 @@ $(function() {
         }
     });
 
+	// menu hamburger
     var api = $('#my-menu').data('mmenu');
     api.bind('opened', function () {
         $('.hamburger').addClass('is-active');
@@ -17,4 +18,23 @@ $(function() {
         $('.hamburger').removeClass('is-active');
     });
 
+    // owl-carousel
+    $('.carousel-services').owlCarousel({
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1100: {
+                items: 3
+            }
+        }
+    });
 });
