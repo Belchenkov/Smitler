3ea8766cb7ee0de23eea5423f249837945608d40
Belchenkov <u608110@gmail.com>
2017-05-14 09:12:53 +0300

    Styling Carousel

--- app/js/common.js
+++ app/js/common.js
@@ -18,9 +18,15 @@ $(function() {
         $('.hamburger').removeClass('is-active');
     });
 
+    $('.carousel-services').on("initialized.owl.carousel", function () {
+        setTimeout( function () {
+            carouselServices();
+        }, 100
+        )
+    });
     // owl-carousel
     $('.carousel-services').owlCarousel({
-        loop: true,
+        //loop: true,
         nav: true,
         smartSpeed: 700,
         navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
@@ -36,5 +42,21 @@ $(function() {
                 items: 3
             }
         }
+    });
+
+    // Setup height of images in carousel
+    function carouselServices() {
+        $('.carousel-services-item').each(function () {
+            var ths = $(this);
+            var thsh = ths.find('.carousel-services-content').outerHeight();
+            var thsi = ths.find('.carousel-services-image').css('min-height', thsh);
+        });
+    }
+    carouselServices();
+
+    // Add span wrapper
+    $('.carousel-services-composition .h3').each(function () {
+        var ths = $(this);
+        ths.html(ths.replace(/(\S+)\s*$/, '<span>$1</span>'));
     });
 });
